d09b81176144939ff604afe65332001d1bd74a6d
GoGetFun <130839858+GoGetFun@users.noreply.github.com>
2025-03-13 12:53:39 +0300

    Update 2.rs

--- 2.rs
+++ 2.rs
@@ -10,9 +10,10 @@ impl Counter {
         Counter { count: 0 }
     }
 
-    fn increment(&mut self) {
-        self.count += 1;
-    }
+    fn increment(&mut self) -> i32 {
+    self.count += 1;
+    self.count
+}
 
     fn get_count(&self) -> i32 {
         self.count
